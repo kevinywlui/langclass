af82954b4289cb9c11cfabb1ae419a3050c19f71
kevin lui <kevinywlui@gmail.com>
2019-10-19 15:56:32 -0700

    Install sklearn

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,3 +1,13 @@
 # This file provides method for vectorizing the input code
 
-from sklearn.utils import murmurhash3_32
+from sklearn.utils import murmurhash
+
+
+class Vectorizer():
+    def __init__(n_features=2**10, n_gram=(1, 1), tokenizer=None, hash_seed=0):
+        self.n_features = n_features
+        self.n_gram = n_gram
+        self.tokenizer = tokenizer
+        self.hash_seed = hash_seed
+
+    def __call__(feature):
