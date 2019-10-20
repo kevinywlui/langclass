b3ced5d26e19c8633cc4e27fa421dd1c98c2afcb
kevin lui <kevinywlui@gmail.com>
2019-10-19 23:22:29 -0700

    Add skeleton for model class

---
+++ src/models/model.py
@@ -0,0 +1,15 @@
+# This file holds the model class which helps abstract away from pre/post
+# processing.
+
+class Model():
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def train(self):
+        pass
+
+    def feature_dict(self):
+        pass
