c1e07a7406bce4d591e39283b4b12d494f3ed730
kevin lui <kevinywlui@gmail.com>
2019-10-23 22:22:24 -0700

    Change get_data to a data class

--- language_source_classifer/data/dataframes.py
+++ language_source_classifer/data/dataframes.py
@@ -3,12 +3,14 @@
 
 import os
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 
 file_path = Path(os.path.realpath(__file__))
 data_path = file_path.parent.parent.parent / "data" / "processed" / "data.csv"
 
-
-def get_df():
-    return pd.read_csv(data_path)
+class Data():
+    def __init__(self):
+        self.full_df = pd.read_csv(data_path)
+        self.train_df, self.test_df = train_test_split(self.full_df, random_state=0)
