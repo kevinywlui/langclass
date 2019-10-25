80e61cfc527a663815cfe0591ef9317b75124d23
kevin lui <kevinywlui@gmail.com>
2019-10-24 22:15:20 -0700

    Add scripts to train and load model

--- langclass/models/predict_model.py
+++ langclass/models/predict_model.py
@@ -1,16 +1,20 @@
 # This script will make a single prediction. HIGHLY INEFFICIENT if you want to
 # make multiple predictions since each prediction will load the model.
 
-import joblib
-from langclass.features.build_features import Vectorizer
-
 import os
 from pathlib import Path
 
-current_file_path = Path(os.path.realpath(__file__))
-vecparams_model_path = current_file_path.parent.parent
+import joblib
 
-def predict(code)
-    params, model = joblib
+from langclass.features.build_features import Vectorizer
+
+current_file_path = Path(os.path.realpath(__file__))
+vecparams_model_path = (
+    current_file_path.parent.parent / "models" / "vecparams_model.pkl"
+)
+
+
+def predict(code):
+    params, model = joblib.load(vecparams_model_path)
     vec = Vectorizer(**params)
     return model.predict([vec(code)])[0]
--- langclass/models/train_model.py
+++ langclass/models/train_model.py
@@ -1,8 +1,34 @@
 # This file trains and save the model.
 # TODO: support more ways to train
 
-import sys
+import os
 from pathlib import Path
 
-save_path = Path(sys.argv[1])
+import lgb
+from langclass.data.dataframes import Data
+from langclass.features.build_features import Vectorizer
 
+current_file_path = Path(os.path.realpath(__file__))
+vecparams_model_path = (
+    current_file_path.parent.parent / "models" / "vecparams_model.pkl"
+)
+
+
+def main():
+    data = Data()
+    train_df = data.train_df
+    X_train = train_df["code"]
+
+    params = {"n_gram": (2, 2), "tokenizer": "char"}
+    vec = Vectorizer(**params)
+
+    X_train_vec = vec.vectorize_df(X_train)
+
+    model = lgb.LGBMClassifier(n_estimators=200)
+    model.fit(X_train_vec, y_train)
+    vecparams_model = (params, model)
+    joblib.dump(vecparams_model, "vecparams_model.pkl")
+
+
+if __name__ == "__main__":
+    main()
