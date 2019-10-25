60e7f9e0af3847748d077e484354541b53f5c0f1
kevin lui <kevinywlui@gmail.com>
2019-10-24 22:14:17 -0700

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='langclass',
+      version='1.0',
+      description='Machine learning model to classify programming languages',
+      author='Kevin Lui',
+      author_email='kevinywlui@gmail.com',
+     )
