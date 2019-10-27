380e59d981ef1852877a1d9e5f09ed3e1dbaf76c
kevin lui <kevinywlui@gmail.com>
2019-10-26 21:01:57 -0700

    First draft of website

--- web/app.py
+++ web/app.py
@@ -7,7 +7,7 @@ from langclass.models.predict_model import Predictor
 
 current_file_path = Path(os.path.realpath(__file__))
 vecparams_model_path = (
-    current_file_path.parent.parent.parent / "models" / "vecparams_model.pkl"
+    current_file_path.parent.parent / "models" / "vecparams_model.pkl"
 )
 
 predictor = Predictor(vecparams_model_path)
@@ -17,10 +17,11 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def langclass_post():
+    language = ""
     if request.method == "POST":
         code = request.form.get("code")
-        return predictor.predict(code)
-    return render_template("index.html", message="Hello Flask!")
+        language = predictor.predict(code)
+    return render_template("index.html", language=language)
 
 
 if __name__ == "__main__":
